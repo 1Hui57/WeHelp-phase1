1176581110008701271594fa6a296b8a8cb089a7
1Hui57 <melody870507@gmail.com>
2025-02-05 19:38:00 +0800

    feat:完成week4 task2

--- week4/main.py
+++ week4/main.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI,Body, Form, Path, Query
+from fastapi import FastAPI,Body, Form, Path, Query, Request
 from fastapi.staticfiles import StaticFiles
 from fastapi.responses import JSONResponse, PlainTextResponse, HTMLResponse, FileResponse, RedirectResponse
 from typing import Annotated
@@ -19,13 +19,16 @@ async def login(
         return {"status":"empty"}
     else:
         return {"status":"error"}
-# return {"account":account,"password":password}
+
+# GET方法將 http://127.0.0.1:8000/member 設為 登入成功畫面
+@app.get("/member")
+def memberPage():
+    return FileResponse("member.html")
+
+# GET方法將/error 設為錯誤頁面
+@app.get("/error") # TemplateResponse已經預設 response_class=HTMLResponse，所以可以不用特別寫
+async def errorPage(request: Request, message:str="error"): # message為要求字串
+    return templates.TemplateResponse("error.html",{"request":request,"message":message})
 
 # 將URL: http://127.0.0.1:8000/設為 index.html
 app.mount("/", StaticFiles(directory="public", html=True))
-
-# 將 http://127.0.0.1:8000/member 設為 登入成功畫面
-@app.get("/member")
-def member():
-    return FileResponse("member.html")
-
